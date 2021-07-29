f80e22949b4b966f40bf4317fb0d471c818bf51b
Toti Muñoz <64804554+totigm@users.noreply.github.com>
2021-07-29 08:38:36 -0300

    🎨 Update uniqueValues.js

--- uniqueValues.js
+++ uniqueValues.js
@@ -1,7 +1,7 @@
 const myArray = [1, 2, 3, 2, 10, 4, 10];
 
 // Get values that aren't repeated
-let getUniqueValues = (array) => [...new Set(array)];
+const getUniqueValues = (array) => [...new Set(array)];
 
 const uniqueValues = getUniqueValues(myArray);
 
